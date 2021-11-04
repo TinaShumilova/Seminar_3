fdb7772c3716a49eabb851c67ba15d5ab0957b6b
Tina Shumilova <shutiva@gmail.com>
2021-11-04 21:45:29 +0200

    add tasks 2,3,4,5

--- Task/Program.cs
+++ Task/Program.cs
@@ -43,8 +43,65 @@ void MyScuare(int a, int b)
 
 }
 
+// нужно как-то  Console.WriteLine из тела метода убрать... у меня ошибки выдаёт
+
 int n = 25;
 int d = 5;
 
 MyScuare(n, d);
 
+//2. Даны два числа. Показать большее и меньшее число
+
+void MaxMin(int a, int b)
+{
+    if(a > b)
+    {
+        Console.WriteLine($" max = {a}, min = {b}");
+    }
+    else
+    {
+        Console.WriteLine($" max = {b}, min = {a}");
+    }
+}
+
+MaxMin(1, 3);
+
+//и здесь тоже.
+
+// 3. По заданному номеру дня недели вывести его название
+
+string[] day = new string[7] { "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday", "Saturday" };
+int w = int.Parse(Console.ReadLine());
+if ((w >= 1) && (w <= 7))
+{
+    Console.WriteLine(day[w - 1]);
+}
+else
+{
+    Console.WriteLine("неверный номер");
+}
+
+//4. Найти максимальное из трех чисел
+
+int m (int a, int b, int c)
+{
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+Console.WriteLine(m(1, 2, 3));
+
+//5. Написать программу вычисления значения функции y=f(a)
+// f = sin
+
+double func(int num)
+{
+    double result = Math.Sin(num);
+    return result;
+}
+
+int a = 30;
+double y = func(a);
+Console.WriteLine(y);
